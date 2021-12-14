de3e39e80031ff9a79c00b028d9d49db7de83b47
Luffka <33230841+luffkaa@users.noreply.github.com>
2021-12-14 05:22:51 +0100

    Added component for reserved time

---
+++ src/components/SchedulerContainer/SingleCompanySchedule/ReservedTime/ReservedTime.tsx
@@ -0,0 +1,32 @@
+import useScheduler from "../../useScheduler";
+import { TimeContainer } from "../TimeContainer";
+import { IReservedTime } from "../types";
+import style from "./index.module.css";
+
+export default function ReservedTime({
+  id,
+}: IReservedTime) {
+  const {
+    handleDeleteSlot,
+    chosenSlot,
+  } = useScheduler({id})
+
+  return (
+    <div
+      className={`module_container box_shadow border_radius_3 ${style.reserved_slot__container}`
+      }
+      onClick={!!chosenSlot ? () => handleDeleteSlot() : undefined}
+    >
+      <h3>Reserved</h3>
+      {!!chosenSlot &&
+        <>
+          <span>{chosenSlot.weekday} </span>
+          <TimeContainer
+            start_time={chosenSlot.start_time}
+            end_time={chosenSlot.end_time}
+          />
+        </>
+      }
+    </div>
+  )
+}
