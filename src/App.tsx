fd6baa172480d9efbd4353b0aca072fb680b8815
Luffka <33230841+luffkaa@users.noreply.github.com>
2021-12-14 05:28:34 +0100

    Added Sheduler container into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -9,7 +8,8 @@ import {
 } from './store';
 import {
   ErrorContainer,
-  PendingContainer
+  PendingContainer,
+  SchedulerContainer,
 } from './components';
 
 function App() {
@@ -31,20 +31,8 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="main__title">Scheduler</h1>
+      <SchedulerContainer />
     </div>
   );
 }
